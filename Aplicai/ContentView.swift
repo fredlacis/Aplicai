55dd4509426e4e12a8251a66e7b0e9f39cef3c04
Fred Lacis <fredlacispro@live.com>
2020-07-24 14:49:22 -0300

    Initial Commit

---
+++ Aplicai/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Aplicai
+//
+//  Created by Frederico Lacis de Carvalho on 24/07/20.
+//  Copyright © 2020 Frederico Lacis de Carvalho. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
